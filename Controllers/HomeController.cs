b4d3b7444b7f8eb4d5f1859982ec8eb8c4a0f8b5
valerie <valerietan000@gmail.com>
2023-01-13 11:06:16 -0800

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Example.Models;
+
+namespace MVC_Example.Controllers;
+
+//Name of controller
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    //constructor
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        string[] days = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        ViewData["days"] = days;
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        //returns home/index.cshtml
+        return View();
+    }
+
+    public IActionResult Bogus()
+    {
+        //Viewdata and Viewbag are interchangeable 
+        ViewData["author"] = "John Cena";
+        ViewBag.Date = DateTime.Now;
+        string[] days = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        return View(days);
+    }
+
+    //called if error
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
